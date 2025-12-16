7c00cab777298fd7e058619e5dfca6d88ea07534
VaibhavShukla2004 <vaibhavshukla1110r@gmail.com>
2025-12-16 18:04:18 +0530

    Few CRUD operations added

--- src/main/java/com/example/demo/controller/QuestionController.java
+++ src/main/java/com/example/demo/controller/QuestionController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +14,28 @@ import com.example.demo.model.Question;
 import com.example.demo.service.QuestionService;
 
 @RestController
-@RequestMapping("question")
+@RequestMapping("/question")
 public class QuestionController {
 
     @Autowired
     QuestionService questionService;
     
-    @GetMapping("allQuestions")
+    @GetMapping("/allQuestions")
     public List <Question> getAllQuestions ()
     {
         return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category)
+    {
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("/add")
+    public String addQuestion(@RequestBody Question question)
+    {
+
+        return questionService.addQuestion(question);
     }
 }
--- src/main/java/com/example/demo/service/QuestionService.java
+++ src/main/java/com/example/demo/service/QuestionService.java
@@ -5,18 +5,30 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.dao.Questiondao;
+import com.example.demo.dao.QuestionDao;
 import com.example.demo.model.Question;
 
 @Service
 public class QuestionService {
 
     @Autowired
-    Questiondao questiondao;
+    QuestionDao questionDao;
 
     public List<Question> getAllQuestions() {
 
-        return questiondao.findAll();
+        return questionDao.findAll();
+    }
+
+    public List<Question> getQuestionsByCategory(String category) {
+        return questionDao.findByCategory(category);
+    }
+
+    public String addQuestion(Question question) {
+        if (question != null) {
+            questionDao.save(question);
+            return "success";
+        }
+        return "failed";
     }
     
 }
